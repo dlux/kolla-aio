ff65c76538d84d761f4fc869917354adff5da61e
root <root@r11s01.cluster.local>
2019-06-25 21:38:31 -0700

    Adding proxy function to host preparation

--- prepare_host.sh
+++ prepare_host.sh
@@ -19,22 +19,26 @@ InstallVagrant (){
     WriteLog "Installing Vagrant"
     vagrant_version="${1:-2.2.4}"
     echo "Installing Vagrant $vagrant_version"
-    wget -q https://releases.hashicorp.com/vagrant/$vagrant_version/vagrant_${vagrant_version}_x86_64.rpm
+    repo="https://releases.hashicorp.com/vagrant/$vagrant_version"
+    wget -q $repo/vagrant_${vagrant_version}_x86_64.rpm
     yum install -y vagrant_${vagrant_version}_x86_64.rpm
     rm vagrant_${vagrant_version}_x86_64.rpm
 }
 
 InstallVirtualBox (){
     WriteLog "Installing VirtualBox"
-    wget -q http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo -P /etc/yum.repos.d
+    repo="http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo"
+    wget -q $repo -P /etc/yum.repos.d
     yum install -y --enablerepo=epel dkms
-    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import -
+    repo="https://www.virtualbox.org/download/oracle_vbox.asc"
+    wget -q $repo -O- | rpm --import -
     yum install -y VirtualBox-5.1
 }
 
 InstallLibvirt (){
     WriteLog "Installing Libvirt"
-    yum install -y qemu libvirt libvirt-devel ruby-devel gcc qemu-kvm nfs-utils nfs-utils-lib
+    yum install -y qemu libvirt libvirt-devel ruby-devel gcc \
+        qemu-kvm nfs-utils nfs-utils-lib
 }
 
 Selinux2Permissive (){
@@ -60,6 +64,17 @@ UpdatePackageManager (){
     yum -y update
 }
 
+SetProxy (){
+    WriteLog "Setting Proxy"
+    prx="http://proxy:port"
+    echo "proxy=$prx" >> /etc/yum.conf
+    npx="127.0.0.0/8,localhost,10.0.0.0/8,192.168.0.0/16"
+    _PROXY="http_proxy=${prx} https_proxy=${prx} no_proxy=${npx}"
+    _PROXY="$_PROXY HTTP_PROXY=${prx} HTTPS_PROXY=${prx} NO_PROXY=${npx}"
+    echo " $_PROXY" | sed "s/ /\nexport /g" > .PROXY
+    source ".PROXY"
+}
+
 InstallPip (){
     if ! which pip; then
         curl -sL https://bootstrap.pypa.io/get-pip.py | python
@@ -78,6 +93,7 @@ InstallCommon (){
 ############################# MAIN FLOW ########################
 
 EnsureRoot
+#SetProxy
 UpdatePackageManager
 Selinux2Permissive
 InstallCommon
